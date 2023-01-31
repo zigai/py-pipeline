416cdf7ea113cb4bd0babeb319a885e0231fef9c
zigai <ziga.ivansek@gmail.com>
2023-01-31 14:09:18 +0100

    update __init__.py

--- pypipeline/__init__.py
+++ pypipeline/__init__.py
@@ -1,5 +1,4 @@
 from pypipeline.filter import *
-from pypipeline.filter import SEP, Filter, FloatFilter, IntFilter
 from pypipeline.pipeline import Pipeline, PriorityPipeline
 from pypipeline.pipeline_action import PipelineAction
 from pypipeline.pipeline_item import PipelineItem
