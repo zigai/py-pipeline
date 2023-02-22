13ce8d2dfbca06b6680935c6d86a741d9129c965
zigai <ziga.ivansek@gmail.com>
2023-02-22 13:01:59 +0100

    extra data

--- pypipeline/pipeline_item.py
+++ pypipeline/pipeline_item.py
@@ -1,6 +1,10 @@
+from typing import Any
+
+
 class PipelineItem:
     def __init__(self) -> None:
         self.discarded = False
+        self.extra_data: dict[str, Any] = {}
 
     def __repr__(self):
         return f"{self.__class__.__name__}(discarded={self.discarded})"
